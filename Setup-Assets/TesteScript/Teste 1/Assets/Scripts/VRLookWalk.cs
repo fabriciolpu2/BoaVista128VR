d949d1cc67073c9d273c575cfc938c93aa60fd25
Michael <mich.mdn21@gmail.com>
2018-08-22 14:22:33 -0400

    testando script vr

---
+++ Setup-Assets/TesteScript/Teste 1/Assets/Scripts/VRLookWalk.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour {
+    public Transform VrCamera;
+
+    public float toggleAngre = 30.0f;
+
+    public float speed = 3.0f;
+
+    public bool moveForward;
+
+    public CharacterController cc;
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+        cc = GetComponentInParent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 if(VrCamera.eulerAngles.x>=toggleAngre && VrCamera.eulerAngles.x< 90.0f)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+
+        }
+
+        if (moveForward)
+        {
+            Vector3 foward = VrCamera.TransformDirection(Vector3.forward);
+
+            cc.SimpleMove(foward * speed);
+        }
+	}
+}
