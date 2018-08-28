f4284e64a95acbfe8fa6d5c0a6250789677e1b7b
Michael <mich.mdn21@gmail.com>
2018-08-28 16:15:03 -0400

    1
    1

--- Setup-Assets/TesteScript/Teste 1/Assets/DesligarTrigger.cs
+++ Setup-Assets/TesteScript/Teste 1/Assets/DesligarTrigger.cs
@@ -6,21 +6,27 @@ public class DesligarTrigger : MonoBehaviour {
     public GameObject player;
     PegarItem script;
     Collider ballcol;
-
+    LayerMask mask;
     private void Start()
     {
         script = player.GetComponent<PegarItem>();
         ballcol = GetComponent<BoxCollider>();
-
+      //  mask = GetComponent<Layer>();
     }
     void Update () {
+
+
+
         if (script.inHands == true )
         {
             ballcol.enabled = false;
+            gameObject.layer = 0;
         }
         else 
         {
+            script.enabled = true;
             ballcol.enabled = true;
+            gameObject.layer = 9;
         }
     }
 }
