74930ce52facdf019fabc3e7b685b1243af5b9f0
Michael <mich.mdn21@gmail.com>
2018-09-06 17:26:57 -0400

    DashBoard OK
    falta adicionar colissor no dashboard

---
+++ Setup-Assets/TesteScript/Teste 1/Assets/InformationScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InformationScript : MonoBehaviour {
+    
+        public GameObject player;
+        public GameObject dashBoard;
+        Transform minhaPosicao;
+        MeshRenderer dashRender;
+        BoxCollider dashCollider;
+        // Use this for initialization
+        void Start()
+        {
+            minhaPosicao = GetComponent<Transform>();
+            dashRender = dashBoard.GetComponent<MeshRenderer>();
+            dashCollider = dashBoard.GetComponent<BoxCollider>();
+        }
+
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            Vector3 direcao = player.transform.position - transform.position;
+            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
+
+        }
+
+        public void ativarDash()
+        {
+            float distancia = Vector3.Distance(minhaPosicao.transform.position, player.transform.position);
+            if (distancia < 4)
+            {
+               dashRender.enabled=true;
+               dashCollider.enabled = true;
+            }
+        }
+
+    }
+
+
