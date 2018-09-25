d8123ba0ef8c5099f529c56156bfb2c4cdd928c4
Fabricio Silva <fabriciolpu2@gmail.com>
2018-09-25 10:24:23 -0400

    draw call - mesh combiner

---
+++ Setup-Assets/BV128VR/Assets/Battlehub/PivotEditor/Scripts/Editor/PivotPointEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Battlehub.MeshTools
+{
+    [CustomEditor(typeof(PivotDesignTime))]
+    public class PivotPointEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            PivotDesignTime pivot = (PivotDesignTime)target;
+
+            GUILayout.BeginHorizontal();
+            {
+                if (GUILayout.Button("To Center Of Mass"))
+                {
+                    Undo.RecordObject(pivot.transform, "Battlehub.PivotPointEditor.CenterOfMass");
+                    pivot.ToCenterOffMass();
+                    EditorUtility.SetDirty(pivot.transform);
+                }
+
+                if (GUILayout.Button("To Bounds Center"))
+                {
+                    Undo.RecordObject(pivot.transform, "Battlehub.PivotPointEditor.BoundsCenter");
+                    pivot.ToBoundsCenter();
+                    EditorUtility.SetDirty(pivot.transform);
+                }
+            }
+            GUILayout.EndHorizontal();
+        }
+    }
+}
+
+
